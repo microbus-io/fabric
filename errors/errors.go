a979dc58386e36e832b6da7152401d97a0a22485
robin <racristob@gmail.com>
2022-09-18 14:48:39 -0700

    remove is and as for now

--- errors/errors.go
+++ errors/errors.go
@@ -26,16 +26,6 @@ func Unwrap(err error) error {
 	return stderrors.Unwrap(err)
 }
 
-// Is is equivalent to the standard Go errors.Is method.
-func Is(err, target error) bool {
-	return stderrors.Is(err, target)
-}
-
-// As is equivalent to the standard Go errors.As method.
-func As(err error, target any) bool {
-	return stderrors.As(err, target)
-}
-
 // New is equivalent to the standard Go errors.New method.
 func New(text string) error {
 	return stderrors.New(text)
