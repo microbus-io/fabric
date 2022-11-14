f35b8957f43dac6759b15dbf300ad268623bd78a
Brian W <45476717+bw19@users.noreply.github.com>
2022-11-14 08:59:49 -0800

    Application enhancements
    Start configurator first.
    Start other services in parallel.
    Simulate crash loop up to 20 seconds, then fail.
    Make thread safe.
    Allow to “join” app.

--- application/application.go
+++ application/application.go
@@ -3,7 +3,9 @@ package application
 import (
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 
 	"github.com/microbus-io/fabric/errors"
 	"github.com/microbus-io/fabric/rand"
@@ -12,97 +14,198 @@ import (
 
 // Application is a collection of microservices that run in a single process and share the same lifecycle.
 type Application struct {
-	services   []Service
-	sig        chan os.Signal
-	plane      string
-	deployment string
+	services       []Service
+	sig            chan os.Signal
+	plane          string
+	deployment     string
+	mux            sync.Mutex
+	startupTimeout time.Duration
 }
 
 // New creates a new app with a collection of microservices.
 // Included microservices must be explicitly started up.
 func New(services ...Service) *Application {
-	return &Application{
-		services: services,
-		sig:      make(chan os.Signal, 1),
+	app := &Application{
+		sig:            make(chan os.Signal, 1),
+		startupTimeout: time.Second * 20,
 	}
+	app.Include(services...)
+	return app
 }
 
 // NewTesting creates a new app for running in a unit test environment.
 // A random plane of communication is used to isolate the app from other apps.
 // Included microservices must be explicitly started up.
 func NewTesting(services ...Service) *Application {
-	return &Application{
-		services:   services,
-		sig:        make(chan os.Signal, 1),
-		plane:      rand.AlphaNum64(8),
-		deployment: "LOCAL",
+	app := &Application{
+		sig:            make(chan os.Signal, 1),
+		plane:          rand.AlphaNum64(8),
+		deployment:     "LOCAL",
+		startupTimeout: time.Second * 20,
 	}
+	app.Include(services...)
+	return app
 }
 
 // Include adds a collection of microservices to the app.
-// The microservices must be explicitly started up even if other microservices added
-// earlier had been started already.
-func (a *Application) Include(services ...Service) {
-	a.services = append(a.services, services...)
+// Added microservices are included in the app's lifecycle management and will be
+// started up or shut down with the app.
+// Inclusion by itself does not startup or shutdown the microservices.
+// Explicit action is required.
+func (app *Application) Include(services ...Service) {
+	app.mux.Lock()
+	for _, s := range services {
+		s.SetPlane(app.plane)
+		s.SetDeployment(app.deployment)
+	}
+	app.services = append(app.services, services...)
+	app.mux.Unlock()
 }
 
-// RemoveByHost shuts down and removes all microservices with the indicated host name.
-func (a *Application) RemoveByHost(host string) error {
-	var lastErr error
-	for j := len(a.services) - 1; j >= 0; j-- {
-		if a.services[j].HostName() != host {
-			continue
+// Join sets the plane and deployment of the microservices to that of the app.
+// This allows microservices to be temporarily joined to the app without being
+// permanently included in its lifecycle management.
+func (app *Application) Join(services ...Service) {
+	for _, s := range services {
+		s.SetPlane(app.plane)
+		s.SetDeployment(app.deployment)
+	}
+}
+
+// Services returns the microservices included in this app.
+// The result is a new array of a limited interface of the microservices
+// that provides means to identify the host of the microservice and start and stop it.
+// Casting is needed in order to access the full microservice functionality.
+func (app *Application) Services() []Service {
+	app.mux.Lock()
+	res := make([]Service, len(app.services))
+	copy(res, app.services)
+	app.mux.Unlock()
+	return res
+}
+
+// Services returns the microservices included in this app that match the host name.
+// The result is a new array of a limited interface of the microservices
+// that provides means to identify the host of the microservice and start and stop it.
+// Casting is needed in order to access the full microservice functionality.
+func (app *Application) ServicesByHost(host string) []Service {
+	app.mux.Lock()
+	res := []Service{}
+	for _, s := range app.services {
+		if s.HostName() == host {
+			res = append(res, s)
 		}
-		if a.services[j].IsStarted() {
-			err := a.services[j].Shutdown()
+	}
+	app.mux.Unlock()
+	return res
+}
+
+// Startup all unstarted microservices included in this app.
+// Configurators are started first, followed by other microservices in parallel.
+// If an error is returned, there is no guarantee as to the state of the microservices:
+// some microservices may have been started while others not.
+func (app *Application) Startup() error {
+	app.mux.Lock()
+	defer app.mux.Unlock()
+
+	// Start configurators first
+	for _, s := range app.services {
+		if s.HostName() == configuratorapi.HostName {
+			err := s.Startup()
 			if err != nil {
-				lastErr = errors.Trace(err)
+				return err
 			}
 		}
-		if !a.services[j].IsStarted() {
-			copy(a.services[j:], a.services[j+1:])
+	}
+
+	// Give services 20 seconds to retry starting up, if needed
+	exit := make(chan bool)
+	timer := time.NewTimer(app.startupTimeout)
+	defer timer.Stop()
+	go func() {
+		<-timer.C
+		close(exit)
+	}()
+
+	// Start services in parallel
+	startErrs := make(chan error, len(app.services))
+	var wg sync.WaitGroup
+	for i, s := range app.services {
+		if s.IsStarted() {
+			continue
+		}
+		s := s
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := errors.New("failed to start")
+			delay := time.Duration(2*i) * time.Millisecond
+			for {
+				select {
+				case <-exit:
+					// Failed to start in 20 seconds, return the last error
+					startErrs <- err
+					return
+				case <-time.After(delay):
+					err = s.Startup()
+					if err == nil {
+						return
+					}
+					delay = app.startupTimeout / 10 // Try again every 2 seconds
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	close(startErrs)
+	var lastErr error
+	for e := range startErrs {
+		if e != nil {
+			lastErr = e
 		}
 	}
 	return lastErr
 }
 
-// Startup all unstarted microservices included in this app.
-// If included, the configurator is started first, then other microservices in order of inclusion.
-func (a *Application) Startup() error {
-	// Move configurator to the front of the list
-	for i, s := range a.services {
-		if s.HostName() != configuratorapi.HostName || i == 0 {
-			continue
-		}
-		copy(a.services[i+1:], a.services[i:])
-		a.services[i] = s
-	}
-	// Start other services in order
-	for _, s := range a.services {
-		if s.IsStarted() {
-			continue
-		}
-		s.SetPlane(a.plane)
-		s.SetDeployment(a.deployment)
-		err := s.Startup()
-		if err != nil {
-			return errors.Trace(err)
-		}
-	}
-	return nil
-}
-
 // Shutdown all started microservices included in this app.
-// If included, the configurator is shut down last, after other microservices in reverse order of inclusion.
-func (a *Application) Shutdown() error {
-	var lastErr error
-	for j := len(a.services) - 1; j >= 0; j-- {
-		if !a.services[j].IsStarted() {
+// The configurator is shut down last, after other microservices in parallel.
+// If an error is returned, there is no guarantee as to the state of the microservices:
+// some microservices may have been shut down while others not.
+func (app *Application) Shutdown() error {
+	app.mux.Lock()
+	defer app.mux.Unlock()
+
+	// Shutdown services in parallel, except for configurators
+	shutdownErrs := make(chan error, len(app.services))
+	var wg sync.WaitGroup
+	for _, s := range app.services {
+		if !s.IsStarted() || s.HostName() == configuratorapi.HostName {
 			continue
 		}
-		err := a.services[j].Shutdown()
-		if err != nil {
-			lastErr = errors.Trace(err)
+		s := s
+		wg.Add(1)
+		go func() {
+			shutdownErrs <- s.Shutdown()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(shutdownErrs)
+	var lastErr error
+	for e := range shutdownErrs {
+		if e != nil {
+			lastErr = e
+		}
+	}
+
+	// Shutdown configurators last
+	for _, s := range app.services {
+		if s.HostName() == configuratorapi.HostName {
+			err := s.Shutdown()
+			if err != nil {
+				lastErr = err
+			}
 		}
 	}
 	return lastErr
@@ -110,24 +213,25 @@ func (a *Application) Shutdown() error {
 
 // WaitForInterrupt blocks until an interrupt is received through
 // a SIGTERM, SIGINT or a call to interrupt.
-func (a *Application) WaitForInterrupt() {
-	signal.Notify(a.sig, syscall.SIGINT, syscall.SIGTERM)
-	<-a.sig
+func (app *Application) WaitForInterrupt() {
+	signal.Notify(app.sig, syscall.SIGINT, syscall.SIGTERM)
+	<-app.sig
 }
 
 // Interrupt the app.
-func (a *Application) Interrupt() {
-	a.sig <- syscall.SIGINT
+func (app *Application) Interrupt() {
+	app.sig <- syscall.SIGINT
 }
 
-// Run all microservices included in this app until an interrupt is received.
-func (a *Application) Run() error {
-	err := a.Startup()
+// Run starts up all microservices included in this app, waits for interrupt,
+// then shuts them down.
+func (app *Application) Run() error {
+	err := app.Startup()
 	if err != nil {
 		return errors.Trace(err)
 	}
-	a.WaitForInterrupt()
-	err = a.Shutdown()
+	app.WaitForInterrupt()
+	err = app.Shutdown()
 	if err != nil {
 		return errors.Trace(err)
 	}
--- application/application_test.go
+++ application/application_test.go
@@ -5,16 +5,20 @@ import (
 	"io"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/microbus-io/fabric/connector"
+	"github.com/microbus-io/fabric/errors"
+	"github.com/microbus-io/fabric/pub"
+	"github.com/microbus-io/fabric/services/configurator"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestApplication_StartStop(t *testing.T) {
 	t.Parallel()
 
-	alpha := connector.New("alpha.startstop.application")
-	beta := connector.New("beta.startstop.application")
+	alpha := connector.New("alpha.start.stop.application")
+	beta := connector.New("beta.start.stop.application")
 	app := New(alpha, beta)
 
 	assert.False(t, alpha.IsStarted())
@@ -65,7 +69,7 @@ func TestApplication_NoConflict(t *testing.T) {
 	ctx := context.Background()
 
 	// Create first testing app
-	alpha := connector.New("noconflict.application")
+	alpha := connector.New("no.conflict.application")
 	alpha.Subscribe("id", func(w http.ResponseWriter, r *http.Request) error {
 		w.Write([]byte("alpha"))
 		return nil
@@ -73,7 +77,7 @@ func TestApplication_NoConflict(t *testing.T) {
 	appAlpha := NewTesting(alpha)
 
 	// Create second testing app
-	beta := connector.New("noconflict.application")
+	beta := connector.New("no.conflict.application")
 	beta.Subscribe("id", func(w http.ResponseWriter, r *http.Request) error {
 		w.Write([]byte("beta"))
 		return nil
@@ -95,7 +99,7 @@ func TestApplication_NoConflict(t *testing.T) {
 
 	// Alpha should never see beta
 	for i := 0; i < 32; i++ {
-		response, err := alpha.GET(ctx, "https://noconflict.application/id")
+		response, err := alpha.GET(ctx, "https://no.conflict.application/id")
 		assert.NoError(t, err)
 		body, err := io.ReadAll(response.Body)
 		assert.NoError(t, err)
@@ -104,10 +108,152 @@ func TestApplication_NoConflict(t *testing.T) {
 
 	// Beta should never see alpha
 	for i := 0; i < 32; i++ {
-		response, err := beta.GET(ctx, "https://noconflict.application/id")
+		response, err := beta.GET(ctx, "https://no.conflict.application/id")
 		assert.NoError(t, err)
 		body, err := io.ReadAll(response.Body)
 		assert.NoError(t, err)
 		assert.Equal(t, []byte("beta"), body)
 	}
+}
+
+func TestApplication_DependencyStart(t *testing.T) {
+	t.Parallel()
+
+	startupTimeout := time.Second * 2
+
+	// Alpha is dependent on beta to start
+	failCount := 0
+	alpha := connector.New("alpha.dependency.start.application")
+	alpha.SetOnStartup(func(ctx context.Context) error {
+		_, err := alpha.Request(ctx, pub.GET("https://beta.dependency.start.application/ok"))
+		if err != nil {
+			failCount++
+			return errors.Trace(err)
+		}
+		return nil
+	})
+
+	// Beta takes a bit of time to start
+	beta := connector.New("beta.dependency.start.application")
+	beta.Subscribe("/ok", func(w http.ResponseWriter, r *http.Request) error {
+		return nil
+	})
+	beta.SetOnStartup(func(ctx context.Context) error {
+		time.Sleep(startupTimeout / 2)
+		return nil
+	})
+
+	app := New(alpha, beta)
+	app.startupTimeout = startupTimeout
+	t0 := time.Now()
+	err := app.Startup()
+	dur := time.Since(t0)
+	assert.NoError(t, err)
+	assert.True(t, failCount > 0)
+	assert.True(t, dur >= startupTimeout/2)
+
+	app.Shutdown()
+}
+
+func TestApplication_FailStart(t *testing.T) {
+	t.Parallel()
+
+	startupTimeout := time.Second
+
+	// Alpha fails to start
+	failCount := 0
+	alpha := connector.New("alpha.fail.start.application")
+	alpha.SetOnStartup(func(ctx context.Context) error {
+		failCount++
+		return errors.New("oops")
+	})
+
+	// Beta starts without a hitch
+	beta := connector.New("beta.fail.start.application")
+
+	app := New(alpha, beta)
+	app.startupTimeout = startupTimeout
+	t0 := time.Now()
+	err := app.Startup()
+	dur := time.Since(t0)
+	assert.Error(t, err)
+	assert.True(t, failCount > 0)
+	assert.True(t, dur >= startupTimeout)
+	assert.True(t, beta.IsStarted())
+	assert.False(t, alpha.IsStarted())
+
+	err = app.Shutdown()
+	assert.NoError(t, err)
+	assert.False(t, beta.IsStarted())
+	assert.False(t, alpha.IsStarted())
+}
+
+func TestApplication_JoinInclude(t *testing.T) {
+	t.Parallel()
+
+	alpha := connector.New("alpha.join.include.application")
+	beta := connector.New("beta.join.include.application")
+	gamma := connector.New("gamma.join.include.application")
+
+	app := NewTesting(alpha)
+	app.Include(beta)
+	app.Join(gamma)
+
+	assert.Equal(t, alpha.Plane(), beta.Plane())
+	assert.Equal(t, alpha.Plane(), gamma.Plane())
+
+	err := app.Startup()
+	assert.NoError(t, err)
+	assert.True(t, alpha.IsStarted())
+	assert.True(t, beta.IsStarted())
+	assert.False(t, gamma.IsStarted())
+
+	err = gamma.Startup()
+	assert.NoError(t, err)
+	assert.True(t, gamma.IsStarted())
+
+	err = app.Shutdown()
+	assert.NoError(t, err)
+	assert.False(t, alpha.IsStarted())
+	assert.False(t, beta.IsStarted())
+	assert.True(t, gamma.IsStarted())
+
+	err = gamma.Shutdown()
+	assert.NoError(t, err)
+	assert.False(t, gamma.IsStarted())
+}
+
+func TestApplication_Services(t *testing.T) {
+	t.Parallel()
+
+	alpha := connector.New("alpha.services.application")
+	beta1 := connector.New("beta.services.application")
+	beta2 := connector.New("beta.services.application")
+
+	app := New(alpha, beta1, beta2)
+	assert.Equal(t, []Service{alpha, beta1, beta2}, app.Services())
+	assert.Equal(t, []Service{beta1, beta2}, app.ServicesByHost("beta.services.application"))
+}
+
+func TestApplication_Run(t *testing.T) {
+	t.Parallel()
+
+	con := connector.New("run.application")
+	config := configurator.NewService()
+	app := New(con, config)
+
+	go func() {
+		err := app.Run()
+		assert.NoError(t, err)
+	}()
+
+	time.Sleep(time.Second)
+	assert.True(t, con.IsStarted())
+	assert.True(t, config.IsStarted())
+
+	app.Interrupt()
+
+	time.Sleep(time.Second)
+	assert.False(t, con.IsStarted())
+	assert.False(t, config.IsStarted())
 }
