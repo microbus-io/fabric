2ef99fbd07b919b2b51ebfb83ef8540c7740c8bb
Brian W <45476717+bw19@users.noreply.github.com>
2023-07-03 17:48:07 -0700

    Adjust license

--- examples/calculator/service.go
+++ examples/calculator/service.go
@@ -1,17 +1,8 @@
 /*
-Copyright 2023 Microbus LLC and various contributors
+Copyright (c) 2022-2023 Microbus LLC and various contributors
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+This file and the project encapsulating it are the confidential intellectual property of Microbus LLC.
+Neither may be used, copied or distributed without the express written consent of Microbus LLC.
 */
 
 package calculator
